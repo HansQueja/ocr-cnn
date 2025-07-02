af645d701a4992f2fb234a46334af4ebfa9a8578
Hans Queja <hansqueja8@gmail.com>
2025-07-02 17:28:58 +0800

    Add main function for either training or predicting

---
+++ main.py
@@ -0,0 +1,19 @@
+import argparse
+
+from helpers.train import train
+from helpers.predict import predict
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--method", help="tells the program if train or predict", type=str, default="train")
+    parser.add_argument("--epochs", help="total number of epochs", type=int, default=1000)
+    parser.add_argument("--pooling", help="if mean, max, or no pooling", type=int, default=0)
+    args = parser.parse_args()
+
+    if args.method == "train":
+        train(args.epochs, args.pooling)
+    else:
+        predict(args.pooling)
+
+if __name__ == "__main__":
+    main()
