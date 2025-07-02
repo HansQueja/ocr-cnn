080c03c9c86fb506035761d5f5c0039af42ab576
Hans Queja <hansqueja8@gmail.com>
2025-07-02 18:57:46 +0800

    Add kernel as argv

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def main():
     parser.add_argument("--method", help="tells the program if train or predict", type=str, default="train")
     parser.add_argument("--epochs", help="total number of epochs", type=int, default=1000)
     parser.add_argument("--pooling", help="if mean, max, or no pooling", type=int, default=0)
+    parser.add_argument("--kernel", help="number of kernels trained", type=int, default=3)
     args = parser.parse_args()
 
     if args.method == "train":
         train(args.epochs, args.pooling)
     else:
-        predict(args.pooling)
+        predict(args.pooling, args.kernel)
 
 if __name__ == "__main__":
     main()
